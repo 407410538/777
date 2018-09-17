3be0c9bea00c67e0bc73b0f199d9f6342e32f1e2
407410538 <j745.rv82@gmail.com>
2018-09-17 18:17:51 +0800

    adding w01 files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 777
+w01 equation.cpp equation.cpp temper convert.cpp
---
+++ w01/temper convert.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	float C, F;
+	printf("Enter Temperature in C:");
+	scanf("%f",&C);
+	F = C*9/5+32;
+	printf("%.1f C = %.1f F\n",C,F);
+	printf("Enter Temperature in F:");
+	scanf("%f",&F);
+    C =(F-32)*5/9;
+	printf("%.1f F = %.1f C\n",F,C);
+	return 0;
+}
+
+
+
